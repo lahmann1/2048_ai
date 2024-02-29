d2fe66c139e725b460d805e1c4563c8a5e04a3bf
lahmann <bjlahmann@gmail.com>
2024-02-28 20:24:28 -0800

    Got game running

--- board.py
+++ board.py
@@ -8,8 +8,11 @@ class Board:
 
     def add_tile(self, value):
         y_indexes, x_indexes = np.where(self.tiles == 0)
+        if len(y_indexes) == 0:
+            return False
         n = np.random.randint(0, len(y_indexes), size=1)
         self.tiles[y_indexes[n], x_indexes[n]] = value
+        return True
 
     def shift_up(self):
         # Get nonzero indexes
@@ -21,21 +24,24 @@ class Board:
         x_indexes = x_indexes[i]
 
         # Loop through all the tiles
+        board_changed = False
         for y, x in zip(y_indexes, x_indexes):
-            collision = False       # Only one collision per tile
             while True:
                 if y == 0:
                     break
                 if self.tiles[y-1][x] == 0:
                     self.tiles[y-1][x] = self.tiles[y][x]
                     self.tiles[y][x] = 0
-                elif self.tiles[y-1][x] == self.tiles[y][x] and not collision:
+                    board_changed = True
+                elif self.tiles[y-1][x] == self.tiles[y][x]:
                     self.tiles[y-1][x] *= 2
                     self.tiles[y][x] = 0
-                    collision = True
+                    board_changed = True
+                    break
                 else:
                     break
                 y -= 1
+        return board_changed
 
     def shift_down(self):
         # Get nonzero indexes
@@ -47,21 +53,24 @@ class Board:
         x_indexes = x_indexes[i]
 
         # Loop through all the tiles
+        board_changed = False
         for y, x in zip(y_indexes, x_indexes):
-            collision = False       # Only one collision per tile
             while True:
                 if y == self.tiles.shape[0] - 1:
                     break
                 if self.tiles[y+1][x] == 0:
                     self.tiles[y+1][x] = self.tiles[y][x]
                     self.tiles[y][x] = 0
-                elif self.tiles[y+1][x] == self.tiles[y][x] and not collision:
+                    board_changed = True
+                elif self.tiles[y+1][x] == self.tiles[y][x]:
                     self.tiles[y+1][x] *= 2
                     self.tiles[y][x] = 0
-                    collision = True
+                    board_changed = True
+                    break
                 else:
                     break
                 y += 1
+        return board_changed
 
     def shift_left(self):
         # Get nonzero indexes
@@ -73,21 +82,24 @@ class Board:
         x_indexes = x_indexes[i]
 
         # Loop through all the tiles
+        board_changed = False
         for y, x in zip(y_indexes, x_indexes):
-            collision = False       # Only one collision per tile
             while True:
                 if x == 0:
                     break
                 if self.tiles[y][x-1] == 0:
                     self.tiles[y][x-1] = self.tiles[y][x]
                     self.tiles[y][x] = 0
-                elif self.tiles[y][x-1] == self.tiles[y][x] and not collision:
+                    board_changed = True
+                elif self.tiles[y][x-1] == self.tiles[y][x]:
                     self.tiles[y][x-1] *= 2
                     self.tiles[y][x] = 0
-                    collision = True
+                    board_changed = True
+                    break
                 else:
                     break
                 x -= 1
+        return board_changed
 
     def shift_right(self):
         # Get nonzero indexes
@@ -99,41 +111,47 @@ class Board:
         x_indexes = x_indexes[i]
 
         # Loop through all the tiles
+        board_changed = False
         for y, x in zip(y_indexes, x_indexes):
-            collision = False       # Only one collision per tile
             while True:
                 if x == self.tiles.shape[1] - 1:
                     break
                 if self.tiles[y][x+1] == 0:
                     self.tiles[y][x+1] = self.tiles[y][x]
                     self.tiles[y][x] = 0
-                elif self.tiles[y][x+1] == self.tiles[y][x] and not collision:
+                    board_changed = True
+                elif self.tiles[y][x+1] == self.tiles[y][x]:
                     self.tiles[y][x+1] *= 2
                     self.tiles[y][x] = 0
-                    collision = True
+                    board_changed = True
+                    break
                 else:
                     break
                 x += 1
+        return board_changed
 
 
 if __name__ == '__main__':
     board = Board()
-    board.add_tile(2)
-    board.add_tile(2)
-    board.add_tile(2)
-    board.add_tile(2)
-    board.add_tile(2)
-    board.add_tile(2)
-    board.add_tile(2)
-    board.add_tile(2)
-    board.add_tile(2)
-    board.add_tile(2)
-    board.add_tile(2)
-    board.add_tile(2)
-    board.add_tile(2)
-    board.add_tile(2)
-    board.add_tile(2)
-    board.add_tile(2)
+    for _ in range(16):
+        board.add_tile(2)
+    while board.shift_up():
+        print(board.tiles)
+
+    board = Board()
+    for _ in range(16):
+        board.add_tile(2)
+    board.shift_down()
     print(board.tiles)
+
+    board = Board()
+    for _ in range(16):
+        board.add_tile(2)
     board.shift_right()
+    print(board.tiles)
+
+    board = Board()
+    for _ in range(16):
+        board.add_tile(2)
+    board.shift_left()
     print(board.tiles)
--- game.py
+++ game.py
@@ -7,12 +7,30 @@ class Game:
 
     def __init__(self, num_starting_tiles=2, spawn_values=(2, 4), spawn_probs=(0.9, 0.1)):
         self.board = Board()
-        values = random.choices(spawn_values, weights=spawn_probs, k=num_starting_tiles)
-        for value in values:
-            self.board.add_tile(value)
+        self.spawn_values = spawn_values
+        self.spawn_probs = spawn_probs
+        for _ in range(num_starting_tiles):
+            self.spawn_tile()
 
-        print(self.board.tiles)
+    def spawn_tile(self):
+        value = random.choices(self.spawn_values, weights=self.spawn_probs)
+        return self.board.add_tile(value)
+
+    def move(self, direction):
+        if direction not in ['up', 'down', 'left', 'right']:
+            raise Exception('Invalid move request')
+        if direction == 'up':
+            return self.board.shift_up() & self.spawn_tile()
+        if direction == 'down':
+            return self.board.shift_down() & self.spawn_tile()
+        if direction == 'left':
+            return self.board.shift_left() & self.spawn_tile()
+        if direction == 'right':
+            return self.board.shift_right() & self.spawn_tile()
 
 
 if __name__ == '__main__':
     game = Game()
+    print(game.board.tiles)
+    while game.move('up'):
+        print(game.board.tiles)
